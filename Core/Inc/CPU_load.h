cfefcfd21e32f189a8305fdf6d3c907f3f50e5cb
Muslum <muslumakbalik@outlook.com>
2023-08-08 15:12:38 +0300

    v4 cpu loa fonksiyonu oluşturuldu

---
+++ Core/Inc/CPU_load.h
@@ -0,0 +1,13 @@
+/*
+ * CPU_load.h
+ *
+ *  Created on: Aug 8, 2023
+ *      Author: Müslüm
+ */
+
+#ifndef INC_CPU_LOAD_H_
+#define INC_CPU_LOAD_H_
+
+
+
+#endif /* INC_CPU_LOAD_H_ */
---
+++ Core/Src/CPU_load.c
@@ -0,0 +1,29 @@
+/*
+ * CPU_load.c
+ *
+ *  Created on: Aug 8, 2023
+ *      Author: Müslüm
+ */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "stdio.h"
+#include "CPU_usage.h"
+#include "usart.h"
+#include "cmsis_os.h"
+#include "usart.h"
+#include "stdio.h"
+#include "CPU_usage.h"
+#include "timers.h"
+
+
+void CPU_Load()
+{
+
+
+
+
+
+
+}
